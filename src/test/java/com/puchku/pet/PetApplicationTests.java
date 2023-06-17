8bfc7ddb7d129bfdc583d9d2a9f017fbf7ffbcd4
namish800 <namishpruthi800@gmail.com>
2023-06-18 04:26:58 +0530

    Commented tests

--- src/test/java/com/puchku/pet/PetApplicationTests.java
+++ src/test/java/com/puchku/pet/PetApplicationTests.java
@@ -1,13 +1,13 @@
-package com.puchku.pet;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PetApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.puchku.pet;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class PetApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
